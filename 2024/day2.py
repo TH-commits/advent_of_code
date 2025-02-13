46a6f2d1829f0b45202fef21bdb257c5b4eb8930
TH-commits <teh217@icloud.com>
2025-02-13 08:52:39 -0800

    Update day2.py

--- 2024/day2.py
+++ 2024/day2.py
@@ -5,17 +5,19 @@ class Historian:
     def __init__(self, file: list[str]) -> None:
         self.file = file
 
-    def clean_file(self) -> int:
+    def answer(self) -> int, int:
         clean_list = [x.split() for x in self.file]
 
         for i, element in enumerate(clean_list):
             clean_list[i] = [int(x) for x in clean_list[i]]
 
-        part1 = self.count_reports(clean_list)
+        part1 = self.part_1(clean_list)
 
-        return part1
+        part2 = self.part_2(clean_list)
 
-    def count_reports(self, clean_list: list[list[int]]) -> int:
+        return part1, part2
+
+    def part_1(self, clean_list: list[list[int]]) -> int:
         count = 0
         for list in clean_list:
             if self.increase_or_decrease(list) and self.adjacent_levels(list):
@@ -38,16 +40,42 @@ class Historian:
     def is_decrease(self, input_list: list[int]) -> bool:
         return all(x > y for x, y in zip(input_list, input_list[1:]))
 
+    def part_2(self, input_list: list[int]) -> int:
+        count = 0
+        for level in input_list:
+            if self.increase_or_decrease(level) and self.adjacent_levels(level):
+                count += 1
+
+            else:
+                if self.case_check(level):
+                    count += 1
+
+        return count
+
+    def case_check(self, level: list[(int, int)]) -> bool:
+        is_valid = False
+        for i, _ in enumerate(level):
+            modified_report = [x for j, x in enumerate(level) if j != i]
+
+            if self.increase_or_decrease(modified_report) and self.adjacent_levels(
+                modified_report
+            ):
+                is_valid = True
+
+        return is_valid
+
 
 def main(filename: str) -> str:
     with open(filename) as outfile:
         file = outfile.read().splitlines()
 
-        clean_file = Historian(file)
+        results = Historian(file)
 
-        answer = clean_file.clean_file()
+        part1, part2 = results.answer()
 
-        print(f"Part 1: {answer}")
+        print(f"Part 1: {part1}")
+
+        print(f"Part 2: {part2}")
 
 
 main(sys.argv[1])
