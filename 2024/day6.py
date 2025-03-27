7593322dcc5dab97faab9f7fbb823575960bc05a
Tyler Hansen <teh217@icloud.com>
2025-03-27 11:32:55 -0700

    Update day6.py

--- 2024/day6.py
+++ 2024/day6.py
@@ -9,23 +9,38 @@ class Guard:
         self.char = "^"
         self.block = "#"
         self.period = "."
+        self._start = self.guard_position()
+        self._path = set()
+
+    def guard_position(self) -> tuple[int, int]:
+        for i, row in enumerate(self.file):
+            for j, c in enumerate(row):
+                if c == self.char:
+                    return (i, j)
+        raise Exception("Oops")
 
     def result(self) -> tuple[int, int]:
         part_2 = 0
-        for i in range(self.rows):
-            for j in range(self.cols):
-                if self.file[i][j] == self.char:
-                    part_1 = self.guard_check(i, j)
-                    if self.loop_check(i, j):
-                        part_2 += 1
+
+        part_1 = self.guard_check(*self._start)
+        grid = [[c for c in row] for row in self.file]
+
+        for i, j in self._path:
+            if (i, j) == self._start:
+                continue
+            # place block
+            grid[i][j] = self.block
+            if self.loop_check(grid, *self._start):
+                part_2 += 1
+            # remove block
+            grid[i][j] = self.period
         return part_1, part_2
 
     def guard_check(self, x: int, y: int) -> int:
         dx, dy = -1, 0
-        visited = set()
 
         while True:
-            visited.add((x, y))
+            self._path.add((x, y))
             if not self.in_bounds(x + dx, y + dy):
                 break
             if self.file[x + dx][y + dy] == self.block:
@@ -34,31 +49,23 @@ class Guard:
                 x += dx
                 y += dy
 
-        return len(visited)
+        return len(self._path)
 
-    def loop_check(self, x: int, y: int) -> int:
+    def loop_check(self, grid: list[list[str]], x: int, y: int) -> bool:
         dx, dy = -1, 0
         visited = set()
 
         while True:
-            print(f"Visiting: {(x, y, dx, dy)}")
             if (x, y, dx, dy) in visited:
-                print("Cycle Detected!")
-                print(f"{(x, y, dy, -dx)}")
                 return True
             visited.add((x, y, dx, dy))
             if not self.in_bounds(x + dx, y + dy):
-                break
-            if self.file[x + dx][y + dy] == self.block:
+                return False
+            if grid[x + dx][y + dy] == self.block:
                 dx, dy = dy, -dx
             else:
                 x += dx
                 y += dy
-
-        print("No Cycle Detected")
-        return False
-
-        # return visited
 
     def in_bounds(self, x: int, y: int) -> bool:
         return 0 <= x < self.rows and 0 <= y < self.cols
